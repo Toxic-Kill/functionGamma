6804eb7d3f2495637905d498e9ef3fe579088d22
Kill Toxic <913087364@qq.com>
2020-05-15 14:52:30 +0800

    functionGamma

--- functionGamma/functionGamma.cpp
+++ functionGamma/functionGamma.cpp
@@ -1,20 +1,47 @@
-﻿// functionGamma.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include<opencv2/opencv.hpp>
+#include<cmath>
 
-#include <iostream>
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//设置矫正系数
+	float gamma = 0.6;
+	//建立查询表数组
+	unsigned char Lut[256];
+	for (int i = 0; i < 256; i++)
+	{
+		Lut[i] = pow((float)(i / 255.0), gamma) * 255;
+	}
+
+
+
+	//读取图像
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\Files\\face.jpg",0);
+	//检测读取成功性
+	if (srcMat.empty())
+	{
+		std::cout << "Fail to open" << std::endl;
+		return -1;
+	}
+
+
+
+	//进行Gamma矫正
+	for (int m = 0; m < srcMat.rows; m++)
+	{
+		for (int n = 0; n < srcMat.cols; n++)
+		{
+			dstMat.at<uchar>(m, n) = Lut[srcMat.at<uchar>(m, n)];
+		}
+	}
+
+
+	//输出图像
+	cv::imshow("dst", dstMat);
+	waitKey(0);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
